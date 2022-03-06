86e2bf68e20448400672fef75df9e42df6252aed
Aleksandr <oleksandr.malishevskyi@gmail.com>
2022-03-07 00:49:35 +0100

    work with auth

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,9 +1,10 @@
 import React, {useContext, useEffect, useState} from 'react'
 import {useHttp} from '../hooks/http.hook'
 import {useMessage} from '../hooks/message.hook'
-/* import AuthContext from '../context/AuthContext' */
+import {AuthContext} from '../context/AuthContext'
 
 const AuthPage = () => {
+  const auth = useContext(AuthContext)
   const message = useMessage()
   const {loading, request, error, clearError} = useHttp()
   const [form, setForm] = useState({
@@ -33,7 +34,7 @@ const AuthPage = () => {
   const loginHandler = async () => {
     try {
       const data = await request('/api/auth/login', 'POST', {...form})
-      message(data.message)
+      auth.login(data.token, data.userId)
     } catch (e) {}
   }
 
