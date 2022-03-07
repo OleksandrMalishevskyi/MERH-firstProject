0af5c6afc51402256e16359143351608c207e09f
Aleksandr <oleksandr.malishevskyi@gmail.com>
2022-03-07 16:04:05 +0100

    cr_back_links

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { useRoutes } from './routes';
 import { useAuth } from './hooks/auth.hook';
 import { AuthContext } from './context/AuthContext'
+import Navbar from './components/Navbar';
 import 'materialize-css';
 
 
@@ -15,6 +16,7 @@ function App() {
       token, login, logout, userId, isAuthenticated
     }}>
       <Router>
+        {isAuthenticated && <Navbar />}
         <div className="container">
           {routes}
         </div>
