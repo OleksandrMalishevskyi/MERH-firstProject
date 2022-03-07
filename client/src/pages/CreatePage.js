e052d75d0848031a37ae40c06ef5fa69086013c8
Aleksandr <oleksandr.malishevskyi@gmail.com>
2022-03-07 21:30:53 +0100

    pipecLinkCard

--- client/src/pages/CreatePage.js
+++ client/src/pages/CreatePage.js
@@ -1,10 +1,10 @@
 import React, {useContext, useEffect, useState} from 'react'
 import {useHttp} from '../hooks/http.hook'
 import {AuthContext} from '../context/AuthContext'
-/* import {useHistory} from 'react-router-dom' */
+import {useHistory} from 'react-router-dom'
 
  const CreatePage = () => {
-/*   const history = useHistory() */
+  const history = useHistory()
   const auth = useContext(AuthContext)
   const {request} = useHttp()
   const [link, setLink] = useState('')
@@ -19,8 +19,7 @@ import {AuthContext} from '../context/AuthContext'
         const data = await request('/api/link/generate', 'POST', {from: link}, {
           Authorization: `Bearer ${auth.token}`
         })
-        console.log(data)
-/*         history.push(`/detail/${data.link._id}`) */
+               history.push(`/detail/${data.link._id}`)
       } catch (e) {}
     }
   }
